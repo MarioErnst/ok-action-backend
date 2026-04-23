5470410784ab517ec212d1d7ef3fbf67871c8e4a
Dayan <dayancorvo@gmail.com>
2026-04-23 09:37:08 -0400

    feat: export all entity models from entities package

---
+++ backend/app/domain/entities/__init__.py
@@ -0,0 +1,15 @@
+from app.domain.entities.role import Role
+from app.domain.entities.user import User
+from app.domain.entities.phonation_session import PhonationSession
+from app.domain.entities.exercise_result import ExerciseResult
+from app.domain.entities.loudness_preset import LoudnessPreset
+from app.domain.entities.loudness_session import LoudnessSession
+
+__all__ = [
+    "Role",
+    "User",
+    "PhonationSession",
+    "ExerciseResult",
+    "LoudnessPreset",
+    "LoudnessSession",
+]
